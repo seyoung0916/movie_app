cfdd31bac9738b1c890ded14fe5d19bcc386adcf
seyoung <seyoung0916@naver.com>
2021-07-24 16:37:17 +0900

    api 호출 및 영화리스트 출력

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import React from 'react';
 import axios from "axios";
+import Movie from './Movie';
+
 class App extends React.Component {
   state = {
     isLoading: true,
     back: false,
   };
 
-  getMovies = async() => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating"); // movies.data.data.movies의 es6
+    console.log(movies);
+    this.setState({ movies, isLoading: false }); // {movies: movies}
   };
+
   componentDidMount() {
     this.getMovies();
   }
+
   render() {
-    const { isLoading } = this.state; // es6
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state; // es6
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} title={title} alt={title}></img>
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h3 class="movie__year">{year}</h3>
+        <p class="movie__summary">{summary} </p>
+      </div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+};
+
+export default Movie;

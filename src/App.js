d9eaaab8cce7905b14d0ee3aa68a2ab4160310ae
seyoung <seyoung0916@naver.com>
2021-07-26 15:34:23 +0900

    라우팅

--- src/App.js
+++ src/App.js
@@ -1,58 +1,15 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About"
+import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    back: false,
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating"); // movies.data.data.movies의 es6
-    console.log(movies);
-    this.setState({ movies, isLoading: false }); // {movies: movies}
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state; // es6
-    return (
-      // className은 html의 class를 나타냄
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => {
-              console.log(movie);
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              );
-            })}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home}></Route>
+    <Route path="/about" component={About}></Route>
+  </HashRouter>
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function About(props) {
+    console.log(props);
+    return <span>About this page: I build it for my toy project.</span>;
+}
+
+export default About;
